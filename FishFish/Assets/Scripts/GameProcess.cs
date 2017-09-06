ea9e4d72fd32f2ec2418d00d9d52f5f0e5891606
Redoxee <anton.francis.roy@gmail.com>
2017-09-07 01:36:47 +0200

    first commit

---
+++ FishFish/Assets/Scripts/GameProcess.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProcess : MonoBehaviour {
+
+    public const int nbControls = 3;
+
+    float[] m_startStamps = new float[nbControls];
+
+    float timer = 0;
+    int[] m_scores = new int[nbControls];
+    
+
+
+    public void OnGameButtonPressed(int index)
+    {
+
+    }
+}
